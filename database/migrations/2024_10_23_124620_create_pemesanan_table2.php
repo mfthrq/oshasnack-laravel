37e40b706ee6b1ae3a332d3850658ca1f3742361
Fadiil <fadilthoriq201@gmail.com>
2024-10-27 11:19:36 +0700

    revisi migration

--- database/migrations/2024_10_23_124620_create_pemesanan_table2.php
+++ database/migrations/2024_10_23_124620_create_pemesanan_table2.php
@@ -13,16 +13,18 @@ class CreatePemesananTable2 extends Migration
      */
     public function up()
     {
-        Schema::create('pemesanan', function (Blueprint $table) {
-            $table->id(); // Kolom id otomatis
-            $table->foreignId('id_pelanggan')->constrained('pelanggans')->onDelete('cascade'); // Foreign key ke tabel pelanggans
-            $table->dateTime('tanggal_pemesanan');
-            $table->integer('total_produk');
-            $table->decimal('total_biaya_transaksi', 10, 2);
-            $table->string('bukti_transaksi')->nullable(); // Gambar bukti transaksi
-            $table->enum('status', ['Diverifikasi', 'Berhasil', 'Gagal'])->default('Diverifikasi'); // Status pemesanan
-            $table->timestamps(); // Kolom created_at dan updated_at
-        });
+        if (!Schema::hasTable('pemesanan')) {
+            Schema::create('pemesanan', function (Blueprint $table) {
+                $table->id();
+                $table->foreignId('id_pelanggan')->constrained();
+                $table->dateTime('tanggal_pemesanan');
+                $table->integer('total_produk');
+                $table->decimal('total_biaya_transaksi', 10, 2);
+                $table->string('bukti_transaksi')->nullable();
+                $table->enum('status', ['Diverifikasi', 'Berhasil', 'Gagal'])->default('Diverifikasi');
+                $table->timestamps();
+            });
+        }
     }
 
     /**
